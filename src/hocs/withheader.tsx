e4f95fb099bc5467159ebb34ff119e8d8540fd10
suvankargiri7 <suvankargiri7@gmail.com>
2025-07-18 16:55:10 +0530

    [src] functionlity of with header hoc

---
+++ src/hocs/withheader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+import TitleComponent from "../components/title";
+
+
+const withHeader = <P extends object>(Component: React.ComponentType<P>) => {
+    return (props:P) => (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <TitleComponent variant="h1" label="Todo App" />
+                </Toolbar>
+            </AppBar>
+            <Component {...props} />
+        </>
+    )
+}
+
+export default withHeader;
