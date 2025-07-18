caa6d36c82138b83fbf45b5351748b5c7b75488b
suvankargiri7 <suvankargiri7@gmail.com>
2025-07-18 17:16:00 +0530

    [src] created basic app

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import withHeader from "./hocs/withheader";
+
+
+const App: React.FC<{}> = () => {
+    return (
+        <>
+            This is app
+        </>
+    )
+};
+
+export default withHeader(App);
+
