ac9681d0c6f5f801a8b57086fc63bddd7d9dea92
suvankargiri7 <suvankargiri7@gmail.com>
2025-07-18 10:55:55 +0530

    [src] created button components

---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, ButtonProps } from "@mui/material";
+
+interface ButtonCompProps extends ButtonProps{
+    label: string
+}
+
+const ButtonComponent: React.FC<ButtonCompProps> = ({label, ...otherProps}) => {
+    return (
+        <Button {...otherProps} aria-label={label}>
+            {label}
+        </Button>
+    )
+}
+
+export default ButtonComponent;
